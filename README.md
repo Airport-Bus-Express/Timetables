bd2dd8a2017370d31ef12a7291911a16d6847b88
Colin <colin@bytesreality.com>
2023-09-29 06:59:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ABE Timetables
+
+The source of timetables for all Airport Bus Express services.
---
+++ xlsx-to-json.js
@@ -0,0 +1,5 @@
+const excelToJson = require('convert-excel-to-json');
+
+const result = excelToJson({
+  sourceFile: 'SOME-EXCEL-FILE.xlsx'
+});
